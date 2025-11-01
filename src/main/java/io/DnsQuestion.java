02343b69614fb748538a6cafdcf523c260174672
phonty29 <amedov.bekmuhamet@gmail.com>
2025-11-01 17:37:13 +0500

    refactor: set labels in constructor

--- src/main/java/io/DnsQuestion.java
+++ src/main/java/io/DnsQuestion.java
@@ -4,13 +4,20 @@ import java.nio.ByteBuffer;
 
 class DnsQuestion implements BufferWrapper {
   private final ByteBuffer questionBuffer;
-  private final short qdCount;
-  private ByteBuffer[] labels;
+  private final ByteBuffer[] labels;
 
   DnsQuestion(ByteBuffer questionBuffer, short questionNumber) {
     this.questionBuffer = questionBuffer.duplicate();
-    this.qdCount = questionNumber;
-    this.setLabels();
+    this.labels = new ByteBuffer[questionNumber];
+    short qIndex = 0, sPos, ePos = 0;
+    while (this.questionBuffer.hasRemaining() && qIndex < questionNumber) {
+      byte nextByte = this.questionBuffer.get();
+      if (nextByte == (byte) 0) {
+        sPos = (short) (ePos + (qIndex > 0 ? 4 : 0));
+        ePos = (short) this.questionBuffer.position();
+        this.labels[qIndex++] = this.questionBuffer.duplicate().position(sPos).limit(ePos).slice();
+      }
+    }
   }
 
   public ByteBuffer getBuffer() {
@@ -19,19 +26,6 @@ class DnsQuestion implements BufferWrapper {
 
   public byte[] getBytes() {
     return this.questionBuffer.array();
-  }
-
-  private void setLabels() {
-    this.labels = new ByteBuffer[this.qdCount];
-    short qIndex = 0, sPos, ePos = 0;
-    while (this.questionBuffer.hasRemaining() && qIndex < this.qdCount) {
-      byte nextByte = this.questionBuffer.get();
-      if (nextByte == (byte) 0) {
-        sPos = (short) (ePos + (qIndex > 0 ? 4 : 0));
-        ePos = (short) this.questionBuffer.position();
-        this.labels[qIndex++] = this.questionBuffer.duplicate().position(sPos).limit(ePos).slice();
-      }
-    }
   }
 
   public ByteBuffer[] getLabels() {
