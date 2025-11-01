a20dd9ca2a7cb877a294c72724db0ec52d5abb88
phonty29 <amedov.bekmuhamet@gmail.com>
2025-11-01 17:31:52 +0500

    refactor: get back first correct solution

--- src/main/java/io/DnsQuestion.java
+++ src/main/java/io/DnsQuestion.java
@@ -5,12 +5,12 @@ import java.nio.ByteBuffer;
 class DnsQuestion implements BufferWrapper {
   private final ByteBuffer questionBuffer;
   private final short qdCount;
-  private final ByteBuffer[] labels;
+//  private final ByteBuffer[] labels;
 
   DnsQuestion(ByteBuffer questionBuffer, short questionNumber) {
     this.questionBuffer = questionBuffer.duplicate();
     this.qdCount = questionNumber;
-    this.labels = retrieveLabels();
+//    this.labels = retrieveLabels();
   }
 
   public ByteBuffer getBuffer() {
@@ -22,21 +22,20 @@ class DnsQuestion implements BufferWrapper {
   }
 
   public ByteBuffer[] getLabels() {
-    return this.labels;
-  }
-
-  private ByteBuffer[] retrieveLabels() {
     this.questionBuffer.position(0);
     ByteBuffer[] labelsBuffer = new ByteBuffer[this.qdCount];
     short qIndex = 0, sPos, ePos = 0;
     while (this.questionBuffer.hasRemaining() && qIndex < this.qdCount) {
       byte nextByte = this.questionBuffer.get();
       if (nextByte == (byte) 0) {
-          sPos = (short) (ePos + (qIndex > 0 ? 4 : 0));
-          ePos = (short) this.questionBuffer.position();
-          labelsBuffer[qIndex++] = this.questionBuffer.duplicate().position(sPos).limit(ePos).slice();
+        sPos = (short) (ePos + (qIndex > 0 ? 4 : 0));
+        ePos = (short) this.questionBuffer.position();
+        labelsBuffer[qIndex++] = this.questionBuffer.duplicate().position(sPos).limit(ePos).slice();
       }
     }
     return labelsBuffer;
   }
+
+//  private ByteBuffer[] retrieveLabels() {
+//  }
 }
