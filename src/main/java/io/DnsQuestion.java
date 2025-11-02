e45cdb3388251093c810b99427786f3fdf34068d
phonty29 <amedov.bekmuhamet@gmail.com>
2025-11-02 21:48:05 +0500

    chore: get labels as it

--- src/main/java/io/DnsQuestion.java
+++ src/main/java/io/DnsQuestion.java
@@ -28,29 +28,52 @@ class DnsQuestion implements BufferWrapper {
     this.questionBuffer.clear();
     ByteBuffer[] labelsBuffer = new ByteBuffer[this.qdCount];
     short qIndex = 0, sPos, ePos = 0;
-    int offset = DnsHeader.SIZE;
-
     while (this.questionBuffer.hasRemaining() && qIndex < this.qdCount) {
-      int currentPosition = this.questionBuffer.position();
       byte nextByte = this.questionBuffer.get();
-      if (nextByte == terminator) {
+      if (nextByte == (byte) 0) {
         sPos = ePos;
-        ePos = (short) this.questionBuffer.position(); // current position
+        ePos = (short) this.questionBuffer.position();
         labelsBuffer[qIndex++] = this.questionBuffer.position(sPos).limit(ePos).slice();
-        ePos += 4;  // skip 4 bytes to the next question
+        ePos += 4;
         this.questionBuffer.position(ePos);
-      }
-      else if (isPointer(nextByte)) {
-        byte restByte = this.questionBuffer.get();
-      } else {
-        // if nextByte = label's length
-        // map each label with its offset
-        labelsMap.put(offset, this.questionBuffer.position(currentPosition).limit(nextByte).slice());
-        this.questionBuffer.position(currentPosition + 1 + nextByte);
-        offset += nextByte + 1;
       }
     }
     return labelsBuffer;
+  }
+
+//  public ByteBuffer[] getLabels() {
+//    this.questionBuffer.clear();
+//    ByteBuffer[] labelsBuffer = new ByteBuffer[this.qdCount];
+//    short qIndex = 0, sPos, ePos = 0;
+//    int offset = DnsHeader.SIZE;
+//
+//    while (this.questionBuffer.hasRemaining() && qIndex < this.qdCount) {
+//      int currentPosition = this.questionBuffer.position();
+//      byte nextByte = this.questionBuffer.get();
+//      if (nextByte == terminator) {
+//        sPos = ePos;
+//        ePos = (short) this.questionBuffer.position(); // current position
+//        labelsBuffer[qIndex++] = this.questionBuffer.position(sPos).limit(ePos).slice();
+//        ePos += 4;  // skip 4 bytes to the next question
+//        this.questionBuffer.position(ePos);
+//      }
+//      else if (isPointer(nextByte)) {
+//        byte restByte = this.questionBuffer.get();
+//        int pointerOffset = getOffsetFromPointer(nextByte, restByte);
+//      } else {
+//        // if nextByte = label's length
+//        // map each label with its offset
+//        labelsMap.put(offset, this.questionBuffer.position(currentPosition).limit(nextByte).slice());
+//        this.questionBuffer.position(currentPosition + 1 + nextByte);
+//        offset += nextByte + 1;
+//      }
+//    }
+//    return labelsBuffer;
+//  }
+
+  private int getOffsetFromPointer(byte nextByte, byte restByte) {
+    short pointer = (short) (((nextByte & 0xFF) << 8) | (restByte & 0xFF));
+    return 0;
   }
 
   private boolean isPointer(byte nextByte) {
